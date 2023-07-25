0472378e64a70520a890c9d739cef4791e616f5a
cnkonica <cnkonica@hotmail.com>
2023-07-26 00:49:33 +0800

    change

---
+++ demo-example/src/main/java/com/cnkonica/demo/example/domain/Student1.java
@@ -0,0 +1,37 @@
+package com.cnkonica.demo.example.domain;
+
+import java.util.Objects;
+
+public class Student1 {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student1 student = (Student1) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
