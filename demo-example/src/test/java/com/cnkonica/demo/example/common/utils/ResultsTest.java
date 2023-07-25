9527c5839e1e4d47e7661cd340c033fb56ac3d6b
cnkonica <cnkonica@hotmail.com>
2023-07-26 00:17:01 +0800

    test case

--- demo-example/src/test/java/com/cnkonica/demo/example/common/utils/ResultsTest.java
+++ demo-example/src/test/java/com/cnkonica/demo/example/common/utils/ResultsTest.java
@@ -3,7 +3,10 @@ package com.cnkonica.demo.example.common.utils;
 import com.cnkonica.demo.example.common.exception.BizResultCode;
 import com.cnkonica.demo.example.common.exception.ResultCode;
 import com.cnkonica.demo.example.common.global.GlobalResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class ResultsTest {
 
@@ -14,52 +17,53 @@ class ResultsTest {
         final GlobalResponse<String> result = Results.success("data");
 
         // Verify the results
+        assertThat(BizResultCode.SUCCESS.getCode()).isEqualTo(result.getCode());
     }
 
-    @Test
-    void testSuccess2() {
-        // Setup
-        // Run the test
-        final GlobalResponse<String> result = Results.success();
-
-        // Verify the results
-    }
-
-    @Test
-    void testError1() {
-        // Setup
-        // Run the test
-        final GlobalResponse<String> result = Results.error();
-
-        // Verify the results
-    }
-
-    @Test
-    void testError2() {
-        // Setup
-        // Run the test
-        final GlobalResponse<String> result = Results.error("message");
-
-        // Verify the results
-    }
-
-    @Test
-    void testError3() {
-        // Setup
-        // Run the test
-        final GlobalResponse<String> result = Results.error("code", "message");
-
-        // Verify the results
-    }
-
-    @Test
-    void testError4() {
-        // Setup
-        final ResultCode resultCode = BizResultCode.SUCCESS;
-
-        // Run the test
-        final GlobalResponse<String> result = Results.error(resultCode);
-
-        // Verify the results
-    }
+//    @Test
+//    void testSuccess2() {
+//        // Setup
+//        // Run the test
+//        final GlobalResponse<String> result = Results.success();
+//
+//        // Verify the results
+//    }
+//
+//    @Test
+//    void testError1() {
+//        // Setup
+//        // Run the test
+//        final GlobalResponse<String> result = Results.error();
+//
+//        // Verify the results
+//    }
+//
+//    @Test
+//    void testError2() {
+//        // Setup
+//        // Run the test
+//        final GlobalResponse<String> result = Results.error("message");
+//
+//        // Verify the results
+//    }
+//
+//    @Test
+//    void testError3() {
+//        // Setup
+//        // Run the test
+//        final GlobalResponse<String> result = Results.error("code", "message");
+//
+//        // Verify the results
+//    }
+//
+//    @Test
+//    void testError4() {
+//        // Setup
+//        final ResultCode resultCode = null;
+//
+//        // Run the test
+//        final GlobalResponse<String> result = Results.error(resultCode);
+//
+//        // Verify the results
+//    }
 }
